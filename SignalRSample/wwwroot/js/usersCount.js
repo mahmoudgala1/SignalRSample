b5660b7af5bba00c83eb9388b1e1bcf55a6e929d
Mahmoud Galal <mahmoudgalal173.95@gmail.com>
2024-09-03 23:10:57 +0300

    Completed

---
+++ SignalRSample/wwwroot/js/usersCount.js
@@ -0,0 +1,24 @@
+﻿var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+connectionUserCount.on("updateTotalViews", (value) => {
+    document.getElementById("totalViewsCounter").innerText = value;
+});
+
+connectionUserCount.on("updateTotalUsers", (value) => {
+    document.getElementById("totalUsersCounter").innerText = value;
+});
+
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+function fulfilled() {
+    console.log("Connection ti User Hub Successful");
+    newWindowLoadedOnClient();
+}
+
+function rejected() {
+    console.log("Connection to User Hub Failed");
+}
+
+connectionUserCount.start().then(fulfilled, rejected);
